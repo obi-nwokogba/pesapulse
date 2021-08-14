0554045f52e660080480a3b0a2690310cbae1772
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2021-08-13 23:58:04 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import UniversalHeader from "./components/UniversalHeader";
 
 function App() {
   return (
     <div className="App">
+      <UniversalHeader />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
       </header>
+      <UniversalFooter />
     </div>
   );
 }
---
+++ src/components/UniversalHeader.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+function UniversalHeader(props){
+    return(
+        <nav className="headerContainer">
+
+            PESAPULSE<br />
+            home
+        </nav>
+    );
+}
+
+
+
+
+export default UniversalHeader;
